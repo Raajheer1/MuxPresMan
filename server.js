6a7faec7dd87edd34739e7b1f46910a01b4638ea
Raajheer1 <41763998+Raajheer1@users.noreply.github.com>
2021-02-03 23:45:05 -0600

    Test

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ var path = require('path');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-//app.use(express.static('public'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs')
 
@@ -38,151 +37,147 @@ function requireHTTPS(req, res, next) {
 app.use(requireHTTPS);
 
 
-// // Setup the Mux SDK
-// const Mux = require('@mux/mux-node');
-// const { Video } = new Mux(process.env.MUX_TOKEN_ID, process.env.MUX_TOKEN_SECRET);
+// Setup the Mux SDK
+const Mux = require('@mux/mux-node');
+const { Video } = new Mux(process.env.MUX_TOKEN_ID, process.env.MUX_TOKEN_SECRET);
 
 
-// let STREAM;
+let STREAM;
 
-// // Storage Configuration
-// const util = require('util');
-// const { response } = require('express');
-// const stateFilePath = './.data/stream';
-// const readFile = util.promisify(fs.readFile);
-// const writeFile = util.promisify(fs.writeFile);
+// Storage Configuration
+const util = require('util');
+const { response } = require('express');
+const stateFilePath = './.data/stream';
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
 
-// // Authentication Configuration
-// const webhookUser = {
-//   name: 'muxer',
-//   pass: 'muxology',
-// };
+// Authentication Configuration
+const webhookUser = {
+  name: 'muxer',
+  pass: 'muxology',
+};
 
-// // Authentication Middleware
-// const auth = (req, res, next) => {
-//   function unauthorized(res) {
-//       res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
-//       return res.send(401);
-//   };
-//   const user = basicAuth(req);
-//   if (!user || !user.name || !user.pass) {
-//       return unauthorized(res);
-//   };
-//   if (user.name === webhookUser.name && user.pass === webhookUser.pass) {
-//       return next();
-//   } else {
-//       return unauthorized(res);
-//   };
-// };
+// Authentication Middleware
+const auth = (req, res, next) => {
+  function unauthorized(res) {
+      res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
+      return res.send(401);
+  };
+  const user = basicAuth(req);
+  if (!user || !user.name || !user.pass) {
+      return unauthorized(res);
+  };
+  if (user.name === webhookUser.name && user.pass === webhookUser.pass) {
+      return next();
+  } else {
+      return unauthorized(res);
+  };
+};
 
-// // Creates a new Live Stream so we can get a Stream Key
-// const createLiveStream = async () => {
-//   if (!process.env.MUX_TOKEN_ID || !process.env.MUX_TOKEN_SECRET) {
-//     console.error("It looks like you haven't set up your Mux token in the .env file yet.");
-//   }
+// Creates a new Live Stream so we can get a Stream Key
+const createLiveStream = async () => {
+  if (!process.env.MUX_TOKEN_ID || !process.env.MUX_TOKEN_SECRET) {
+    console.error("It looks like you haven't set up your Mux token in the .env file yet.");
+  }
 
-//   // Create a new Live Stream!
-//   return await Video.LiveStreams.create({
-//     playback_policy: 'public',
-//     reconnect_window: 10,
-//     new_asset_settings: { playback_policy: 'public' } 
-//   });
-// };
+  // Create a new Live Stream!
+  return await Video.LiveStreams.create({
+    playback_policy: 'public',
+    reconnect_window: 10,
+    new_asset_settings: { playback_policy: 'public' } 
+  });
+};
 
-// // Reads a state file looking for an existing Live Stream, if it can't find one, 
-// // creates a new one, saving the new live stream to our state file and global
-// // STREAM variable.
-// const initialize = async () => {
-//   try {
-//     const stateFile = await readFile(stateFilePath, 'utf8');
-//     STREAM = JSON.parse(stateFile);
-//     console.log('Found an existing stream! Fetching updated data.');
-//     STREAM = await Video.LiveStreams.get(STREAM.id);
-//   } catch (err) {
-//     console.log('No stream found, creating a new one.');
-//     STREAM = await createLiveStream();
-//     await writeFile(stateFilePath, JSON.stringify(STREAM));
-//   }
-//   return STREAM;
-// }
+// Reads a state file looking for an existing Live Stream, if it can't find one, 
+// creates a new one, saving the new live stream to our state file and global
+// STREAM variable.
+const initialize = async () => {
+  try {
+    const stateFile = await readFile(stateFilePath, 'utf8');
+    STREAM = JSON.parse(stateFile);
+    console.log('Found an existing stream! Fetching updated data.');
+    STREAM = await Video.LiveStreams.get(STREAM.id);
+  } catch (err) {
+    console.log('No stream found, creating a new one.');
+    STREAM = await createLiveStream();
+    await writeFile(stateFilePath, JSON.stringify(STREAM));
+  }
+  return STREAM;
+}
 
-// // Lazy way to find a public playback ID (Just returns the first...)
-// const getPlaybackId = stream => stream['playback_ids'][0].id;
+// Lazy way to find a public playback ID (Just returns the first...)
+const getPlaybackId = stream => stream['playback_ids'][0].id;
 
-// // Gets a trimmed public stream details from a stream for use on the client side
-// const publicStreamDetails = stream => ({
-//   status: stream.status,
-//   playbackId: getPlaybackId(stream),
-//   recentAssets: stream['recent_asset_ids'],
-// })
+// Gets a trimmed public stream details from a stream for use on the client side
+const publicStreamDetails = stream => ({
+  status: stream.status,
+  playbackId: getPlaybackId(stream),
+  recentAssets: stream['recent_asset_ids'],
+})
 
-// // API for getting the current live stream and its state for bootstrapping the app
-// app.get('/stream', async (req, res) => {
-//   const stream = await Video.LiveStreams.get(STREAM.id);
-//   res.json(
-//     publicStreamDetails(stream)
-//   );
-// });
+// API for getting the current live stream and its state for bootstrapping the app
+app.get('/stream', async (req, res) => {
+  const stream = await Video.LiveStreams.get(STREAM.id);
+  res.json(
+    publicStreamDetails(stream)
+  );
+});
 
-// // API which Returns the 5 most recent VOD assets made from our Live Stream
-// app.get('/recent', async (req, res) => {
-//   const recentAssetIds = STREAM['recent_asset_ids'] || [];
+// API which Returns the 5 most recent VOD assets made from our Live Stream
+app.get('/recent', async (req, res) => {
+  const recentAssetIds = STREAM['recent_asset_ids'] || [];
 
-//   // For each VOD asset we know about, get the details from Mux Video
-//   const assets = await Promise.all(
-//     recentAssetIds
-//     .reverse()
-//     .slice(0, 5)
-//     .map((assetId) =>
-//       Video.Assets.get(assetId).then(asset => {
+  // For each VOD asset we know about, get the details from Mux Video
+  const assets = await Promise.all(
+    recentAssetIds
+    .reverse()
+    .slice(0, 5)
+    .map((assetId) =>
+      Video.Assets.get(assetId).then(asset => {
 
-//         return {
-//           playbackId: getPlaybackId(asset),
-//           status: asset.status,
-//           createdAt: asset.created_at,
-//         };
-//       })
-//     )
-//   );
-//   res.json(assets);
-// });
+        return {
+          playbackId: getPlaybackId(asset),
+          status: asset.status,
+          createdAt: asset.created_at,
+        };
+      })
+    )
+  );
+  res.json(assets);
+});
 
-// // API which Listens for callbacks from Mux
-// app.post('/mux-hook', auth, function (req, res) {
-//   STREAM.status = req.body.data.status;
+// API which Listens for callbacks from Mux
+app.post('/mux-hook', auth, function (req, res) {
+  STREAM.status = req.body.data.status;
   
-//   switch (req.body.type) {
+  switch (req.body.type) {
 
       
-//     // When a stream goes idle, we want to capture the automatically created 
-//     // asset IDs, so we can let people watch the on-demand copies of our live streams
-//     case 'video.live_stream.idle':
-//       STREAM['recent_asset_ids'] = req.body.data['recent_asset_ids'];
-//       // We deliberately don't break; here
+    // When a stream goes idle, we want to capture the automatically created 
+    // asset IDs, so we can let people watch the on-demand copies of our live streams
+    case 'video.live_stream.idle':
+      STREAM['recent_asset_ids'] = req.body.data['recent_asset_ids'];
+      // We deliberately don't break; here
 
-//     // When a Live Stream is active or idle, we want to push a new event down our
-//     // web socket connection to our frontend, so that it update and display or hide
-//     // the live stream.
-//     case 'video.live_stream.active':
-//       io.emit('stream_update', publicStreamDetails(STREAM));
-//       break;
-//     default:
-//       // Relaxing.
-//   }
+    // When a Live Stream is active or idle, we want to push a new event down our
+    // web socket connection to our frontend, so that it update and display or hide
+    // the live stream.
+    case 'video.live_stream.active':
+      io.emit('stream_update', publicStreamDetails(STREAM));
+      break;
+    default:
+      // Relaxing.
+  }
 
-//   res.status(200).send('Thanks, Mux!');
-// });
+  res.status(200).send('Thanks, Mux!');
+});
 
-// // Starts the HTTP listener for our application.
-// // Note: glitch helpfully remaps HTTP 80 and 443 to process.env.PORT
-// initialize().then((stream) => {
-//   const listener = https.listen(process.env.PORT || 443, function() {
-//     console.log('Your app is listening on port ' + listener.address().port);
-//     console.log('HERE ARE YOUR STREAM DETAILS, KEEP THEM SECRET!');
-//     console.log(`Stream Key: ${stream.stream_key}`);
-//   });
-// });
-
-https.listen(443, () => {
-  console.log("ITS RUNNING");
-})
+// Starts the HTTP listener for our application.
+// Note: glitch helpfully remaps HTTP 80 and 443 to process.env.PORT
+initialize().then((stream) => {
+  const listener = https.listen(process.env.PORT || 443, function() {
+    console.log('Your app is listening on port ' + listener.address().port);
+    console.log('HERE ARE YOUR STREAM DETAILS, KEEP THEM SECRET!');
+    console.log(`Stream Key: ${stream.stream_key}`);
+  });
+});
