816132a359a9b643fadc40cebdabd9576e0de2c4
Raajheer1 <41763998+Raajheer1@users.noreply.github.com>
2021-02-03 23:33:35 -0600

    updated routes

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.static('public'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs')
 
-app.get("/", (req, res) => {
+app.get("/index", (req, res) => {
   console.log(stream.stream_key)
   res.render('index', {
     stream: STREAM.id
